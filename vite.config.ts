69bd7881dcedc842ae609b53b2cf79da12a74714
Dr T <tosin@Mac-109.Home>
2026-03-26 05:25:40 +0000

    Standardize CSS and PostCSS config to fix build errors

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,9 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
       "@shared": path.resolve(__dirname, "shared"),
-      "@assets": path.resolve(__dirname, "attached_assets")
     }
   },
   root: path.resolve(__dirname, "client"),
-  publicDir: path.resolve(__dirname, "client", "public"),
   build: {
     outDir: path.resolve(__dirname, "dist/public"),
     emptyOutDir: true
